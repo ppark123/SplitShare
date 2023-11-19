63d2b4a1fe5db5a7f571605cf0b891b2cce47561
mayaarafa <146292273+mayaarafa@users.noreply.github.com>
2023-11-19 15:24:30 -0800

    pending nav bar + buttons

--- frontend/pending.js
+++ frontend/pending.js
@@ -1,14 +1,14 @@
 function remove1() {
-    let div = document.getElementsByClassName("pendingDonation1");
+    let div = document.getElementById("1");
     div.style.display = "none";
 }
 
-function remove2(divName) {
-
-    document.getElementsByClassName("divName").style.display = "none";
+function remove2() {
+    let div = document.getElementById("2");
+    div.style.display = "none";
 }
 
-function remove3(divName) {
-
-    document.getElementsByClassName("divName").style.display = "none";
+function remove3() {
+    let div = document.getElementById("3");
+    div.style.display = "none";
 }
