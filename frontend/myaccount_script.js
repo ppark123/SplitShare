50354a52297568d6f08532d06d3aea5e8856f8eb
ppark123 <pearlpark8798@gmail.com>
2023-11-19 15:17:28 -0800

    adding increment

--- frontend/myaccount_script.js
+++ frontend/myaccount_script.js
@@ -1,3 +1,8 @@
-function goBackHome(){
+function goBackHome() {
     window.location.href = "./index.html";
+}
+
+function incrementDonated() {
+    number=document.getElementById("donated");
+    number=parseInt(number.innerHTML) + 1;
 }
