d25bb639f2f74dbb43f81b0356d43a0d8a91f3b5
c1217h03 <charm17halim@gmail.com>
2023-11-19 17:30:57 -0800

    added pop up window when you post

--- backend/create_post.js
+++ backend/create_post.js
@@ -1,3 +1,13 @@
 function redirectToHome() {
     window.location.href = "../frontend/index.html";
 }
+
+function showPopup() {
+    document.getElementById('postPopup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+  
+  function closePopup() {
+    document.getElementById('postPopup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+  }
