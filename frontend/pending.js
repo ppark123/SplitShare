5a5a29246586dd448b09d38f6a396a441ca36558
ppark123 <pearlpark8798@gmail.com>
2023-11-19 16:05:24 -0800

    fixed account

--- frontend/pending.js
+++ frontend/pending.js
@@ -12,3 +12,22 @@ function remove3(divName) {
 
     document.getElementsByClassName("divName").style.display = "none";
 }
+
+function remove1_completed(divName) {
+    incrementDonated()
+    document.getElementsByClassName("divName").style.display = "none";
+
+
+}
+
+function remove2_completed(divName) {
+    incrementDonated()
+    document.getElementsByClassName("divName").style.display = "none";
+    
+}
+
+function remove3_completed(divName) {
+    incrementDonated()
+    document.getElementsByClassName("divName").style.display = "none";
+
+}
