f8a05f5bd3fcfef4367fc6c532493bc246881cd4
Pearl Park <108495732+ppark123@users.noreply.github.com>
2023-11-19 16:09:23 -0800

    Update pending.js
    deleted changes

--- frontend/pending.js
+++ frontend/pending.js
@@ -8,32 +8,7 @@ function remove2() {
     div.style.display = "none";
 }
 
-<<<<<<< HEAD
-function remove3(divName) {
-
-    document.getElementsByClassName("divName").style.display = "none";
-}
-
-function remove1_completed(divName) {
-    incrementDonated()
-    document.getElementsByClassName("divName").style.display = "none";
-
-
-}
-
-function remove2_completed(divName) {
-    incrementDonated()
-    document.getElementsByClassName("divName").style.display = "none";
-    
-}
-
-function remove3_completed(divName) {
-    incrementDonated()
-    document.getElementsByClassName("divName").style.display = "none";
-
-=======
 function remove3() {
     let div = document.getElementById("3");
     div.style.display = "none";
->>>>>>> 809ce0cf9ba888a92d980af82aebdeb0ffd2ec10
 }
